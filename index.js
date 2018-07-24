9226a36db0304c5d244b27d20e3b241968f7e373
Daniel Merz <Daniel.Merz@Actano.de>
2018-07-24 13:37:49 +0200

    message to all channels with webhook

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
 
-const sendWebhookMessage = require('./src/webhook-client')
+const { sendMessage, sendMessageToAll} = require('./src/webhook-client')
+const sendRtmMessage = require('./src/rtm-client')
 
 const ids = {
     conversationId: 'oVlmyq1xE2yGZ7nSaHCKS3EF',
     personalMessageToMe: 'DBVKYLTJP',
     personalChannelWithWorkspace: 'TBVGRV5FW/BBVUCQ1A9/oVlmyq1xE2yGZ7nSaHCKS3EF',
+    generalChannel: 'CBV7KF10R',
 }
 
+// sendMessage('Hello there! ... Ah General Kenobi!', ids.personalMessageToMe)
+sendMessageToAll('Message to all').catch(console.error)
 
-sendWebhookMessage('Hello there! ... Ah General Kenobi!', ids.personalMessageToMe)
-
+// sendRtmMessage('Mega Message', ids.generalChannel)
--- src/webhook-client.js
+++ src/webhook-client.js
@@ -1,20 +1,34 @@
 const { WebClient } = require('@slack/client');
 
 // An access token (from your Slack app or custom integration - xoxp, xoxb, or xoxa)
-const token = process.env.SLACK_TOKEN;
+const token = process.env.SLACK_BOT_USER_TOKEN
 const web = new WebClient(token);
 console.log('webhook-client connected')
 
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
+// web.channels.list().then( res => console.log(res.channels)).catch(err => {console.log('err web.channel()'); console.error(err)})
 
+async function sendMessageToAll(msg) {
+    const channelListResult = await web.channels.list()
+    const writableChannelsIds = channelListResult.channels.filter(c => c.is_member).map(c => c.id)
+
+    await Promise.all(
+        writableChannelsIds.map(
+            channel => web.chat.postMessage({ channel, text: msg })
+        )
+    )
+}
 const sendMessage = (msg, channelId) => {
     // See: https://api.slack.com/methods/chat.postMessage
     web.chat.postMessage({ channel: channelId, text: msg })
         .then((res) => {
             // `res` contains information about the posted message
-            console.log('Message sent: ', res.ts);
+            console.log('webhook Message sent: ', res.ts);
         })
-        .catch(console.error);
+        .catch((err) => {console.log('error in webhook.sendMessage: '); console.error(err)});
 }
 
-module.exports = sendMessage
+module.exports = {
+    sendMessage,
+    sendMessageToAll,
+}
