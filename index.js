8d141937830bc1cb1a8c7402afb0f523c53c15fc
Daniel Merz <Daniel.Merz@Actano.de>
2018-07-24 13:09:52 +0200

    channelId as param

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const ids = {
     personalChannelWithWorkspace: 'TBVGRV5FW/BBVUCQ1A9/oVlmyq1xE2yGZ7nSaHCKS3EF',
 }
 
-// An access token (from your Slack app or custom integration - xoxp, xoxb, or xoxa)
-const token = process.env.SLACK_TOKEN;
 
-sendWebhookMessage('Hello there! ... Ah General Kenobi!')
+sendWebhookMessage('Hello there! ... Ah General Kenobi!', ids.personalMessageToMe)
+
--- src/webhook-client.js
+++ src/webhook-client.js
@@ -1,20 +1,15 @@
 const { WebClient } = require('@slack/client');
 
-const ids = {
-    conversationId: 'oVlmyq1xE2yGZ7nSaHCKS3EF',
-    personalMessageToMe: 'DBVKYLTJP',
-    personalChannelWithWorkspace: 'TBVGRV5FW/BBVUCQ1A9/oVlmyq1xE2yGZ7nSaHCKS3EF',
-}
-
+// An access token (from your Slack app or custom integration - xoxp, xoxb, or xoxa)
 const token = process.env.SLACK_TOKEN;
 const web = new WebClient(token);
-console.log('connected')
+console.log('webhook-client connected')
 
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
 
-const sendMessage = (msg) => {
+const sendMessage = (msg, channelId) => {
     // See: https://api.slack.com/methods/chat.postMessage
-    web.chat.postMessage({ channel: ids.personalMessageToMe, text: msg })
+    web.chat.postMessage({ channel: channelId, text: msg })
         .then((res) => {
             // `res` contains information about the posted message
             console.log('Message sent: ', res.ts);
